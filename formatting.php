cea4dedae96161937508082ae228e426352ff709
khalidyatim19 <2020489056@student.uitm.edu.my>
2022-04-25 15:45:47 +0800

    six commit

--- formatting.php
+++ formatting.php
@@ -8,13 +8,13 @@
 </head>
 <body>
 <?php
-printf("%.2f", 123.156);
+printf("%.2f", 123.456);
 echo "<br />";
 printf("%.10f", 123.456);
 echo "‹br />";
 printf("$.0f", 123.456);
 echo "<br />";
-echo number_format ("2500000",2) ;
+echo number_format("2500000",2);
 ?>
 </body>
 </html>
